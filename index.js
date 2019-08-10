456c5232cecc2145c854b1339c91161a4ec0dac4
Jonathan Luk <jluk87@gmail.com>
2019-08-10 21:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ class Driver {
 }
 
 class Route {
+  constructor (beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
 
 
 }
